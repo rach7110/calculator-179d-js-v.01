1789ad51fe7dfee7ba7477663b46919e7705232a
Rachel <rachel.loziuk@gmail.com>
2015-12-13 11:15:34 -0600

    calculate and display tax benefit when user submits

--- public/calculator.js
+++ public/calculator.js
@@ -1,70 +1,76 @@
 // window.onload = (function(){
 //   alert("Works!");
 // });
+
 $(document).ready(function(){
-  event.preventDefault();
-  $('#user-results').hide();
-  $(':submit').click(function(){
-    $('#user-results').show(); 
-    $('div#benefit-results').empty();
-    var size = document.getElementById("building-size").value;
-    var name = document.getElementById("name").value;
+  // alert("Works!");
 
-    var lighting = false;
-    var hvac = false;
-    var env = false;
-    var lightingLength = $('input#lighting:checked').length;
-    var hvacLength = $('input#hvac:checked').length;
-    var envLength = $('input#env:checked').length;
+    event.preventDefault();
+    $('#user-results').hide();
 
-    var rateLighting = 0.60;
-    var rateHvac = 0.60;
-    var rateEnv = 0.60;
+    $(':submit').click(function(){
+        $('#user-results').show(); 
+        $('div#benefit-result').empty();
+        $('div#size-result').empty();
 
-    var benefitLighting = 0;
-    var benefitHvac = 0;
-    var benefitEnv = 0;
+        var size = document.getElementById("building-size").value;
+        var name = document.getElementById("name").value;
 
-    // Checks the user entered a building size:
-    if(size =="") {
-      alert("Please enter a building size");
-      return;
-    //Checks the user entered a building size greater than or equal to 100,000 sqft: 
-    } else if(parseInt(size) < 100000) {
-        alert("Building size must be larger than 100,000 sqft");
-        return;
-    //Checks the user entered a name: 
-    } else if(name.length == 0) {
-        alert("Please eneter your name");
-        return;     
-    } else {
+        // DEFAULT VALUES
+        // var lighting = false;
+        // var hvac = false;
+        // var env = false;
+
+        var benefitLighting = 0;
+        var benefitHvac = 0;
+        var benefitEnv = 0;
+
+        // CHECK IF USER SELECTED ANY OF THE EFFICIENCY SYSTEMS
+        var lighting = $('input#lighting:checked').length;
+        var hvac = $('input#hvac:checked').length;
+        var env = $('input#env:checked').length;
+
+        var rateLighting = 0.60;
+        var rateHvac = 0.60;
+        var rateEnv = 0.60;
+        
+        // VALIDATE BUILDING SIZE
+        if(size =="") {
+          alert("Please enter a building size");
+          return;
+        //Checks the user entered a building size greater than or equal to 100,000 sqft: 
+        } else if(parseInt(size) < 100000) {
+            alert("Building size must be larger than 100,000 sqft");
+            return;
+        //Checks the user entered a name: 
+        // } else if(name.length == 0) {
+            // alert("Please eneter your name");
+            // return;     
+        };
+
         var size = parseInt(size);
-    };
 
-// Checks if the lighting checkbox is selected:
-    // alert("('input#lighting:checked').length is:  " +  lightingLength);
-    if (lightingLength > 0) {
-      lighting = true;
-      benefitLighting = size * rateLighting;
-    };
-// Checks if the HVAC checkbox is selected:
-    // alert("('input#hvac:checked').length is:  " +  hvacLength);
-    if (hvacLength > 0) {
-      hvac = true;
-      benefitHvac = size * rateHvac;
-    };
-// Checks if the envelope checkbox is selected:
-    // alert("('input#env:checked').length is:  " +  envLength);
-    if(envLength > 0) {
-      env = true;
-      benefitEnv = size * rateEnv;
-    };
+        // CALCULATE LIGHTING TAX BENEFIT
+        // alert("('input#lighting:checked').length is:  " +  lighting);
+          benefitLighting = size * rateLighting * lighting;
+        // CALCULATE HVAC TAX BENEFIT
+        // alert("('input#hvac:checked').length is:  " +  hvac);
+          benefitHvac = size * rateHvac * hvac;
+        
+        // CALCULATE HVAC ENVELOPE BENEFIT
+        // alert("('input#env:checked').length is:  " +  env);
+          benefitEnv = size * rateEnv * env;
 
-    if(lighting == false && hvac == false && env == false) {
-      alert("You must select at least one buidling efficiency to qualify");
-    } else {
-    var totalBenefits = (benefitLighting + benefitHvac + benefitEnv);
-    $('div#benefit-results').show().append('<strong>Your total potential tax beenfit: $ </strong>' + totalBenefits);
-    };
-  });
+        var totalBenefits = (benefitLighting + benefitHvac + benefitEnv);
+
+
+        if(lighting == false && hvac == false && env == false) {
+          alert("You must select at least one buidling efficiency to qualify");
+        } else {
+        // DISPLAY RESULTS
+        $('div#size-result').append('<strong>' +  size + ' </strong>');
+        $('div#benefit-result').append('<strong>$' + totalBenefits + ' </strong>');
+        };
+
+    });
 });
